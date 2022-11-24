5124042b5e43168255724e48befc9713783d9aa0
Juho <81303814+JHSic@users.noreply.github.com>
2022-11-24 14:40:15 +0900

    Update 도망못가 - 트리거.sql

--- DDL(TABLE, PROCEDURE, TRIGGER)/도망못가 - 트리거.sql
+++ DDL(TABLE, PROCEDURE, TRIGGER)/도망못가 - 트리거.sql
@@ -2,25 +2,25 @@
 DELIMITER $
 CREATE TRIGGER Make_Recruit_By_Absence
 	AFTER UPDATE
-    ON Absence
-    FOR EACH ROW
-    BEGIN
-		IF new.absence_state = 1 THEN
-		INSERT INTO Recruit(work_type_index, recruit_state, work_start, work_end, recruit_worker, applyment_worker) 
-			values ((SELECT work_type_index FROM User WHERE user_index = old.user_index), 1,  new.absence_start, new.absence_end, 1, 0);
-		END IF;
-	END;
-DELIMETER ;
+    	ON Absence
+    	FOR EACH ROW
+    		BEGIN
+			IF new.absence_state = 1 THEN
+				INSERT INTO Recruit(work_type_index, recruit_state, work_start, work_end, recruit_worker, applyment_worker) 
+				values ((SELECT work_type_index FROM User WHERE user_index = old.user_index), 1,  new.absence_start, new.absence_end, 1, 0);
+			END IF;
+		END;
+DELIMETER;
 
 # 초과 근무 승인 시 신청 인원 증가하고 가득 찰 경우 모집 완료
 DELIMITER $
 CREATE TRIGGER Complete_Apply_Recruit
 	AFTER UPDATE
-    ON Overtime
-    FOR EACH ROW
-    BEGIN
-	IF new.cover_state = 1 THEN
-        CALL UpdateRecruit(new.recruit_index);
-        END IF;
-	END;
+   	ON Overtime
+    	FOR EACH ROW
+    		BEGIN
+			IF new.cover_state = 1 THEN
+				CALL UpdateRecruit(new.recruit_index);
+			END IF;
+		END;
 DELIMITER;
